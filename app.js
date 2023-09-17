ed689b00bd00d3acbb919de11df426735ce939a0
Faozia Islam Riha <96914762+riha29@users.noreply.github.com>
2023-09-18 00:16:44 +0600

    posting update

--- app.js
+++ app.js
@@ -1,22 +1,3 @@
-// function loadPosts(){
-//     fetch('https://jsonplaceholder.typicode.com/posts')
-//     .then(res => res.json())
-//     .then(data=> displayPosts(data))
-// }
-
-// loadPosts()
-
-// function displayPosts(data){
-//     const postContainer= document.getElementById("posting-post")
-//     for (const i of data){
-//         const div= document.createElement('div')
-//         div.innerHTML= `
-//         <h3>${i.title}</h3>`
-//         postContainer.appendChild(div)
-//         console.log(i.title)
-//     }
-// }
-
 document.getElementById('post-button').addEventListener('click', function() {
     const postInput= document.getElementById('post-input')
     const postText= postInput.value
